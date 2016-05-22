20ef541ec4aa925ed24cd67881d8dd3080cfb68d
Chiao Cheng <chiao@alum.mit.edu>
2016-05-22 18:20:34 +0800

    setup for batching train loop NN calls

--- main.py
+++ main.py
@@ -7,40 +7,6 @@ from model import Game
 import neural2048
 
 
-def game_loop(game, model, epsilon):
-    while True:
-        choices = [(i, x)
-                   for i, x in enumerate(game.post_states())
-                   if x is not None]
-        if len(choices) == 0:
-            break
-        moves, states = zip(*choices)
-        scores = model.predict(np.array(states)).flatten()
-        if random.random() > epsilon:
-            chosen_move, chosen_state = choices[scores.argmax()]
-        else:
-            chosen_move, chosen_state = random.choice(choices)
-        reward = game.move(chosen_move)
-        yield chosen_state, reward
-
-
-def gl2srs(gl):
-    state0 = None
-    for state1, reward in gl:
-        state1 = np.array(state1)
-        if state0 is not None:
-            yield (state0, reward, state1)
-        state0 = state1
-    yield (state0, 0., np.zeros_like(state0))
-
-
-def score2hist(scores):
-    score_hist = defaultdict(int)
-    for score0 in scores:
-        score_hist[score0] += 1
-    return score_hist
-
-
 transforms = [
     lambda x: x,
     lambda x: np.rot90(x, k=1),
@@ -50,6 +16,70 @@ transforms = [
     lambda x: np.rot90(np.fliplr(x), k=2),
     lambda x: np.rot90(np.fliplr(x), k=3),
 ]
+
+
+class game_loop():
+    def __init__(self, epsilon):
+        self.epsilon = epsilon
+        self.game = Game()
+        self.done = False
+        self.state = None
+        self.SRSs = []
+        self.generate_choices()
+
+    def generate_choices(self):
+        self.choices = [
+            (i, x)
+            for i, x in enumerate(self.game.post_states())
+            if x is not None
+        ]
+
+    def get_states(self):
+        if self.done:
+            return np.empty(0)
+
+        self.moves, states = zip(*self.choices)
+
+        return np.array(states)
+
+    def move(self, scores):
+        if self.done:
+            return
+
+        if random.random() > self.epsilon:
+            chosen_move, chosen_state = self.choices[scores.flatten().argmax()]
+        else:
+            chosen_move, chosen_state = random.choice(self.choices)
+
+        chosen_reward = self.game.move(chosen_move)
+
+        if self.state is not None:
+            for transform0 in transforms:
+                self.SRSs.append((
+                    transform0(self.state),
+                    self.reward,
+                    transform0(chosen_state)
+                ))
+
+        self.state = chosen_state
+        self.reward = chosen_reward
+
+        self.generate_choices()
+        if len(self.choices) == 0:
+            self.done = True
+            for transform0 in transforms:
+                self.SRSs.append((
+                    transform0(chosen_state),
+                    chosen_reward,
+                    np.zeros_like(chosen_state)
+                ))
+
+
+def score2hist(scores):
+    score_hist = defaultdict(int)
+    for score0 in scores:
+        score_hist[score0] += 1
+    return score_hist
 
 
 def main():
@@ -69,16 +99,14 @@ def main():
             if len(SRSs) - counter > 100000:
                 counter = len(SRSs)
                 print "len(SRSs): {0:8d} / {1:8d}".format(counter, 1000000)
-            game = Game()
-            gl = game_loop(game, model, epsilon)
-            for state0, reward, state1 in gl2srs(gl):
-                for transform0 in transforms:
-                    SRSs.append((
-                        transform0(state0),
-                        reward,
-                        transform0(state1)
-                    ))
-            scores.append(game.score)
+            gl = game_loop(epsilon)
+            while True:
+                states = gl.get_states()
+                if len(states) == 0:
+                    SRSs.extend(gl.SRSs)
+                    break
+                gl.move(model.predict(states))
+            scores.append(gl.game.score)
 
         score_hist = score2hist(scores)
         print sorted(score_hist.items())
